8c270aee84661b722445f2457bba762eecdd6bef
DarkKnight054 <omarsultan054@gmail.com>
2024-01-17 02:08:00 +0600

    Change in decodedFiles.js

--- utils/decodeFiles.js
+++ utils/decodeFiles.js
@@ -24,15 +24,7 @@
 // }
 
 // module.exports = { decodeFile };
-
-
-// --------------------- BOTTLENECK MECHANISM ------------------------
-
-const fs = require('fs');
-const Bottleneck = require('bottleneck');
-
-// Create a bottleneck with a specified limit
-const limiter = new Bottleneck({ maxConcurrent: 1, minTime: 100 });
+const fs = require('fs').promises;
 
 async function decodeFile(jsonData) {
     const dataArr = jsonData.content.split('::');
@@ -42,25 +34,59 @@ async function decodeFile(jsonData) {
     const chunkSize = 99999; // 1 Megabyte
     const decodedData = Buffer.from(base64, 'base64');
 
-    const writeFileAsync = (file, data, options) => {
-        return new Promise((resolve, reject) => {
-            fs.writeFile(file, data, options, (err) => {
-                if (err) reject(err);
-                else resolve();
-            });
-        });
-    };
-
     for (let i = 0; i < decodedData.length; i += chunkSize) {
         const chunkEnd = i + chunkSize < decodedData.length ? i + chunkSize : decodedData.length;
         const chunk = Buffer.alloc(chunkEnd - i);
         decodedData.copy(chunk, 0, i, chunkEnd);
 
-        // Use the bottleneck to limit the rate of writes
-        await limiter.schedule(() => writeFileAsync(`received-files/${file_name}`, chunk, { flag: 'a' }));
-        
-        console.log(`Chunk written successfully`);
+        try {
+            // Use async/await with fs.promises.writeFile for asynchronous file writing
+            await fs.writeFile(`received-files/${file_name}`, chunk, { flag: 'a' });
+            console.log(`Chunk written successfully`);
+        } catch (err) {
+            console.error('Error writing chunk to file:', err);
+        }
     }
 }
 
 module.exports = { decodeFile };
+
+
+// --------------------- BOTTLENECK MECHANISM ------------------------
+
+// const fs = require('fs');
+// const Bottleneck = require('bottleneck');
+
+// // Create a bottleneck with a specified limit
+// const limiter = new Bottleneck({ maxConcurrent: 1, minTime: 1000 });
+
+// async function decodeFile(jsonData) {
+//     const dataArr = jsonData.content.split('::');
+//     const file_name = dataArr[0];
+//     const base64 = dataArr[1];
+
+//     const chunkSize = 99999; // 1 Megabyte
+//     const decodedData = Buffer.from(base64, 'base64');
+
+//     const writeFileAsync = (file, data, options) => {
+//         return new Promise((resolve, reject) => {
+//             fs.writeFile(file, data, options, (err) => {
+//                 if (err) reject(err);
+//                 else resolve();
+//             });
+//         });
+//     };
+
+//     for (let i = 0; i < decodedData.length; i += chunkSize) {
+//         const chunkEnd = i + chunkSize < decodedData.length ? i + chunkSize : decodedData.length;
+//         const chunk = Buffer.alloc(chunkEnd - i);
+//         decodedData.copy(chunk, 0, i, chunkEnd);
+
+//         // Use the bottleneck to limit the rate of writes
+//         await limiter.schedule(() => writeFileAsync(`received-files/${file_name}`, chunk, { flag: 'a' }));
+        
+//         console.log(`Chunk written successfully`);
+//     }
+// }
+
+// module.exports = { decodeFile };
